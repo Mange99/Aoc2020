f3efb4c72b2e3ccebb45fc87306589c79efa03d5
mange99 <magnus.bryntheson@hotmail.com>
2020-12-01 09:01:07 +0100

    day one done

--- src/problem1/main.java
+++ src/problem1/main.java
@@ -20,13 +20,19 @@ public class main {
         while(sc.hasNextInt()){
             numbers.add(sc.nextInt());
         }
-        printList(numbers);
-    }
-    public void printList(List<Integer> list){
-        for(int i = 0; i < list.size(); i++){
-            System.out.println(list.get(i));
+        for(int i = 0; i < numbers.size(); i++){
+            for(int j = 1; j < numbers.size(); j++){
+                for(int n = 2; n < numbers.size(); n++){
+                    if(numbers.get(i) + numbers.get(j) + numbers.get(n) == 2020 && i != j && i != n){
+                        System.out.println("i: " + numbers.get(i) + " j " + numbers.get(j) + " " + numbers.get(n));
+                        System.out.println(numbers.get(i) * numbers.get(j) * numbers.get(n));
+                        System.out.println(i + " " + j + " " + n);
+                    }
+                }
+            }
         }
     }
+
     public static void main(String[] args) throws FileNotFoundException {
         new main();
     }
